2edf36b25cf300cb5099b15811171c7d603a2878
NekoNekoNyan <me@neko-dev.ru>
2024-01-30 10:31:22 +0300

    device reorder

--- src/inc/lib/hub/device.js
+++ src/inc/lib/hub/device.js
@@ -17,6 +17,7 @@ class Device {
     api_v: 0,
     platform: '',
   };
+  #input_queue;
 
   // device
   focused = false;
@@ -33,62 +34,11 @@ class Device {
   granted = false;
   cfg_flag = false;
 
-  inq = new InputQueue(1000, 1000);
-
   constructor(hub, id) {
     this._hub = hub;
     this.info = hub.config.getDevice(id);
+    this.#input_queue = new InputQueue(1000, 1000);  // TODO config
   }
-
-  //#region Connection
-
-  /**
-   * Check if device is accessable by http.
-   * @returns {boolean}
-   */
-  isHttpAccessable() {
-    for (const connection of this.active_connections) {
-      if (connection instanceof HTTPconn)
-        return true;
-    }
-    return false;
-  }
-
-  /**
-   * Get primary connection
-   * @returns {Connection}
-   */
-  getConnection() {
-    return this.active_connections.maxBy(conn => conn.priority);
-  }
-
-  /**
-   * Check if device is connected to hub.
-   * @returns {boolean}
-   */
-  isConnected() {
-    return this.active_connections.length !== 0;
-  }
-
-  /**
-   * Add new active connection to device.
-   * @param {Connection} conn 
-   */
-  addConnection(conn) {
-    if (this.active_connections.includes(conn)) return;
-
-    this.active_connections.push(conn);
-
-    conn.addEventListener('statechange', e => {
-      switch (e.state) {
-        case ConnectionState.DISCONNECTED:
-          this.active_connections.remove(conn);
-          break;
-      }
-    })
-  }
-
-  //#endregion
 
   /**
    * Check if module is enabled
@@ -98,6 +48,8 @@ class Device {
   isModuleEnabled(mod) {
     return !(this.info.modules & mod);
   }
+
+  //#region Communication
 
   /**
    * Send command to device
@@ -122,37 +74,9 @@ class Device {
     }
   }
 
-  async focus() {
-    this.focused = true;
-    await this.#postAndWait('ui', ['ok']);
-    // if (this.conn == Conn.HTTP && this.info.ws_port) {
-    //   this.ws.connect();
-    //   setTimeout(() => {
-    //     if (!this.ws.state()) this.ws.disconnect();
-    //   }, this.tout_prd);
-    // }
-  }
-
-  async unfocus() {
-    this.focused = false;
-    this._stop_ping();
-    this._stop_tout();
-    await this.post('unfocus');
-    // if (this.conn == Conn.HTTP) this.ws.disconnect();
-  }
-
-  async getInfo() {
-    const [type, data] = await this.#postAndWait('info', ['info', 'ok']);
-    if (type === 'info') return data;
-    return undefined;
-  }
-
-  async reboot() {
-    await this.#postAndWait('reboot', ['ok']);
-  }
-
-  async sendCli(command) {
-    await this.#postAndWait('cli', ['ok'], 'cli', command);
+  async #postAndWait(cmd, types, name = '', value = '') {
+    await this.post(cmd, name, value);
+    return await this.#input_queue.get(types);
   }
 
   _stop_tout() {
@@ -185,8 +109,6 @@ class Device {
     }, this.ping_prd);
   }
 
-  // private
-
   _checkUpdates(updates) {
     if (typeof(updates) != 'object') return;
     for (let name in updates) {
@@ -201,11 +123,6 @@ class Device {
     }
   }
 
-  async #postAndWait(cmd, types, name = '', value = '') {
-    await this.post(cmd, name, value);
-    return await this.inq.get(types);
-  }
-
   async _parse(type, data, conn) {
     let id = this.info.id;
     this._stop_tout();
@@ -215,7 +132,7 @@ class Device {
       if (this.focused) this._hub.onDeviceConnChange(id, true);
     }
 
-    this.inq.put(type, data);
+    this.#input_queue.put(type, data);
 
     /**
      * ping -> ok
@@ -284,50 +201,162 @@ class Device {
     }
   }
 
-//#region OTA
+  //#endregion
 
-  async uploadOta(file, type, progress = undefined) {
-    if (!this.isModuleEnabled(Modules.OTA)) 
-      throw new Error(HubErrors.Disabled);
-  
-    if (this.fsBusy())
-      throw new Error(HubErrors.FsBusy);
+  //#region Connection
 
-    if (this.isHttpAccessable() && this.info.http_t) {
-      let formData = new FormData();
-      formData.append(type, file);
-      await http_post(`http://${this.info.ip}:${this.info.http_port}/hub/ota?type=${type}&client_id=${this._hub.clientId}`, formData)
-    } else {
-      if (!progress) progress = () => {};
+  /**
+   * Check if device is accessable by http.
+   * @returns {boolean}
+   */
+  isHttpAccessable() {
+    for (const connection of this.active_connections) {
+      if (connection instanceof HTTPconn)
+        return true;
+    }
+    return false;
+  }
 
-      const fdata = await readFileAsArrayBuffer(file);
-      const buffer = new Uint8Array(fdata);
-      const ota_bytes = Array.from(buffer);
-      const ota_size = ota_bytes.length;
-      const max_enc_len = this.info.max_upl * 3 / 4 - 60;
+  /**
+   * Get primary connection
+   * @returns {Connection}
+   */
+  getConnection() {
+    return this.active_connections.maxBy(conn => conn.priority);
+  }
 
-      let [cmd, data] = await this.#postAndWait('ota', ['ota_next', 'ota_done', 'ota_err'], type);
+  /**
+   * Check if device is connected to hub.
+   * @returns {boolean}
+   */
+  isConnected() {
+    return this.active_connections.length !== 0;
+  }
 
-      while (cmd === 'ota_next') {
-        const data2 = String.fromCharCode.apply(null, ota_bytes.splice(0, max_enc_len));
-        progress(Math.round((ota_size - ota_bytes.length) / ota_size * 100));
-        [cmd, data] = await this.#postAndWait('ota_chunk', ['ota_next', 'ota_done', 'ota_err'], (ota_bytes.length) ? 'next' : 'last', window.btoa(data2));
+  /**
+   * Add new active connection to device.
+   * @param {Connection} conn 
+   */
+  addConnection(conn) {
+    if (this.active_connections.includes(conn)) return;
+
+    this.active_connections.push(conn);
+
+    conn.addEventListener('statechange', e => {
+      switch (e.state) {
+        case ConnectionState.DISCONNECTED:
+          this.active_connections.remove(conn);
+          break;
       }
+    })
+  }
 
-      if (cmd === 'ota_err')
-        throw new Error(data.code);
+  //#endregion
+
+  //#region UI files
+
+  files = [];
+  async _fetchFiles() {
+    while (this.files) {
+      let file = this.files.shift();
+  
+      Widget.setPlabel(file.name, '[FETCH...]');
+      let res;
+      try {
+        res = await this.fetch(file.path, perc => {
+          Widget.setPlabel(file.name, `[${perc}%]`);
+        });
+      } catch (e) {
+        Widget.setPlabel(file.name, '[ERROR]');
+        return;
+      }
+      file.callback(`data:${getMime(file.path)};base64,${res}`);
     }
   }
 
-  async otaUrl(type, url) {
-    const [t, data] = await this.#postAndWait('ota_url', ['ota_url_ok', 'ota_url_err'], type, url);
-    if (t === 'ota_url_err')
-      throw new Error(data.code);
+  file_flag = false;
+  resetFiles() {
+    this.files = [];
+    this.file_flag = false;
   }
 
-//#endregion
+  async addFile(name, path, callback) {
+    let has = this.files.some(f => f.name == name);
+    if (!has) this.files.push({
+      name, path, callback,
+    });
+    if (this.file_flag && this.files.length == 1) await this._fetchFiles();
+  }
 
-//#region FS
+  async checkFiles() {
+    this.file_flag = true;
+    await this._fetchFiles();
+  }
+
+  //#endregion
+
+  //#region Interraction > Common
+
+  async focus() {
+    this.focused = true;
+    await this.#postAndWait('ui', ['ok']);
+    // if (this.conn == Conn.HTTP && this.info.ws_port) {
+    //   this.ws.connect();
+    //   setTimeout(() => {
+    //     if (!this.ws.state()) this.ws.disconnect();
+    //   }, this.tout_prd);
+    // }
+  }
+
+  async unfocus() {
+    this.focused = false;
+    this._stop_ping();
+    this._stop_tout();
+    await this.post('unfocus');
+    // if (this.conn == Conn.HTTP) this.ws.disconnect();
+  }
+
+  async getInfo() {
+    const [type, data] = await this.#postAndWait('info', ['info', 'ok']);
+    if (type === 'info') return data;
+    return undefined;
+  }
+
+  async reboot() {
+    await this.#postAndWait('reboot', ['ok']);
+  }
+
+  async sendCli(command) {
+    await this.#postAndWait('cli', ['ok'], 'cli', command);
+  }
+
+  //#endregion
+
+  //#region Interraction > FS
+
+  async deleteFile(path) {
+    res = await this.#postAndWait('delete', ['files'], path);
+  }
+
+  async createFile(path) {
+    res = await this.#postAndWait('mkfile', ['files'], path);
+  }
+
+  async renameFile(path, new_name) {
+    res = await this.#postAndWait('rename', ['files'], path, new_name);
+  }
+
+  async formatFS() {
+    res = await this.#postAndWait('format', ['files']);
+  }
+
+  async updateFileList() {
+    res = await this.#postAndWait('files', ['files']);
+  }
+
+  async #onFiles(data) {
+    this._hub.onFsbr(id, data.fs, data.total, data.used);
+  }
 
   async upload(file, path, progress = undefined) {
     if (!this.isModuleEnabled(Modules.UPLOAD)) 
@@ -417,71 +446,49 @@ class Device {
         throw new Error(data.code);
     }
   }
-
-  async deleteFile(path) {
-    res = await this.#postAndWait('delete', ['files'], path);
-  }
-
-  async createFile(path) {
-    res = await this.#postAndWait('mkfile', ['files'], path);
-  }
-
-  async renameFile(path, new_name) {
-    res = await this.#postAndWait('rename', ['files'], path, new_name);
-  }
-
-  async formatFS() {
-    res = await this.#postAndWait('format', ['files']);
-  }
-
-  async updateFileList() {
-    res = await this.#postAndWait('files', ['files']);
-  }
-
-  async #onFiles(data) {
-    this._hub.onFsbr(id, data.fs, data.total, data.used);
-  }
-
-//#endregion
-
-  files = [];
-
-  async _fetchFiles() {
-    while (this.files) {
-      let file = this.files.shift();
   
-      Widget.setPlabel(file.name, '[FETCH...]');
-      let res;
-      try {
-        res = await this.fetch(file.path, perc => {
-          Widget.setPlabel(file.name, `[${perc}%]`);
-        });
-      } catch (e) {
-        Widget.setPlabel(file.name, '[ERROR]');
-        return;
+  //#endregion
+
+  //#region Interraction > OTA
+
+  async otaUrl(type, url) {
+    const [t, data] = await this.#postAndWait('ota_url', ['ota_url_ok', 'ota_url_err'], type, url);
+    if (t === 'ota_url_err')
+      throw new Error(data.code);
+  }
+
+  async uploadOta(file, type, progress = undefined) {
+    if (!this.isModuleEnabled(Modules.OTA)) 
+      throw new Error(HubErrors.Disabled);
+  
+    if (this.fsBusy())
+      throw new Error(HubErrors.FsBusy);
+
+    if (this.isHttpAccessable() && this.info.http_t) {
+      let formData = new FormData();
+      formData.append(type, file);
+      await http_post(`http://${this.info.ip}:${this.info.http_port}/hub/ota?type=${type}&client_id=${this._hub.clientId}`, formData)
+    } else {
+      if (!progress) progress = () => {};
+
+      const fdata = await readFileAsArrayBuffer(file);
+      const buffer = new Uint8Array(fdata);
+      const ota_bytes = Array.from(buffer);
+      const ota_size = ota_bytes.length;
+      const max_enc_len = this.info.max_upl * 3 / 4 - 60;
+
+      let [cmd, data] = await this.#postAndWait('ota', ['ota_next', 'ota_done', 'ota_err'], type);
+
+      while (cmd === 'ota_next') {
+        const data2 = String.fromCharCode.apply(null, ota_bytes.splice(0, max_enc_len));
+        progress(Math.round((ota_size - ota_bytes.length) / ota_size * 100));
+        [cmd, data] = await this.#postAndWait('ota_chunk', ['ota_next', 'ota_done', 'ota_err'], (ota_bytes.length) ? 'next' : 'last', window.btoa(data2));
       }
-      file.callback(`data:${getMime(file.path)};base64,${res}`);
+
+      if (cmd === 'ota_err')
+        throw new Error(data.code);
     }
   }
 
-  // file
-  file_flag = false;
-
-  resetFiles() {
-    this.files = [];
-    this.file_flag = false;
-  }
-
-  async addFile(name, path, callback) {
-    let has = this.files.some(f => f.name == name);
-    if (!has) this.files.push({
-      name, path, callback,
-    });
-    if (this.file_flag && this.files.length == 1) await this._fetchFiles();
-  }
-
-  async checkFiles() {
-    this.file_flag = true;
-    await this._fetchFiles();
-  }
+  //#endregion
 };
