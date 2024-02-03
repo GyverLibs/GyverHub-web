8db202bb8dec49f95ac8bcaacd049d497d7baf35
NekoNekoNyan <me@neko-dev.ru>
2024-02-03 12:46:50 +0300

    image

---
+++ src/inc/lib/hubui/widgets/image.js
@@ -0,0 +1,28 @@
+class ImageWidget extends BaseWidget {
+    $el;
+    #path;
+
+    constructor(data, renderer) {
+        super(data, renderer);
+
+        this.makeLayout({
+            type: 'div',
+            name: 'el',
+            html: waiter(),
+        });
+        
+        this.update(data);
+    }
+
+    update(data) {
+        super.update(data);
+        if ('value' in data) this.#path = data.value;
+        if ('action' in data || 'value' in data) {
+            hub.dev(focused).addFile(id, this.#path, file => {
+                this.setPlabel();
+                this.$el.innerHTML = `<img style="width:100%" src="${file}">`;
+            }, perc => this.setPlabel(`[${perc}%]`));
+        }
+    }
+}
+Renderer.register('image', ImageWidget);
