ea45ed241a4be652a336a7b6f5bb2e03260c2554
NekoNekoNyan <me@neko-dev.ru>
2024-02-06 13:03:52 +0300

    popup

--- src/inc/popup.js
+++ src/inc/popup.js
@@ -1,93 +1,110 @@
-function makeDialog() {
-  let box = document.createElement('div');
-  box.className = 'dialog_box';
-  document.body.appendChild(box);
-  return box;
+function makeDialog(title, text, buttons, additional) {
+  const $box = document.createElement('div');
+  $box.className = 'dialog_box';
+  document.body.appendChild($box);
+
+  const $d = document.createElement('div');
+  $box.append($d);
+  $d.className = 'ui_col ui_dialog';
+
+  if (title) {
+    const $title = document.createElement('div');
+    $d.append($title);
+    $title.className = 'ui_row ui_head';
+    $title.textContent = title;
+  }
+
+  if (text) {
+    const $text = document.createElement('div');
+    $d.append($text);
+    $text.className = 'ui_row';
+
+    const $label = document.createElement('label');
+    $text.append($label);
+    $label.className = 'dialog_row';
+    $label.textContent = text;
+  }
+
+  if (additional) {
+    const $text = document.createElement('div');
+    $d.append($text);
+    $text.className = 'ui_row';
+    $text.innerHTML = additional;
+  }
+
+  if (buttons) {
+    const $buttons = document.createElement('div');
+    $d.append($buttons);
+    $buttons.className = 'ui_row';
+
+    $buttons.append(document.createElement('div'));
+
+    const $row = document.createElement('div');
+    $buttons.append($row);
+    $row.className = 'ui_btn_row';
+
+    for (const i of buttons) {
+      const $btn = document.createElement('button');
+      $row.append($btn);
+      $btn.className = 'ui_btn ui_btn_mini';
+      $btn.textContent = i.text;
+      $btn.addEventListener('click', i.click);
+    }
+  }
+  return $box;
 }
 
 function asyncAlert(text, title = null) {
   return new Promise(resolve => {
-    let box = makeDialog();
-    let tit = title ? `<div class="ui_row ui_head">${title}</div>` : '';
-    box.innerHTML = `
-    <div class="ui_col ui_dialog">
-      ${tit}
-      <div class="ui_row">
-        <label class="dialog_row">${text}</label>
-      </div>
-      <div class="ui_row">
-        <div></div>
-        <button id="alert_btn" class="ui_btn ui_btn_mini">OK</button>
-      </div>
-    </div>`;
-
-    EL('alert_btn').onclick = () => {
-      document.body.removeChild(box);
-      resolve(true);
-    };
+    let box = makeDialog(title, text, [{
+      text: 'OK',
+      click:() => {
+        document.body.removeChild(box);
+        resolve(true);
+      }
+    }]);
   });
 }
 
 function asyncConfirm(text, title = null) {
   return new Promise(resolve => {
-    let box = makeDialog();
-    let tit = title ? `<div class="ui_row ui_head">${title}</div>` : '';
-    box.innerHTML = `
-    <div class="ui_col ui_dialog">
-      ${tit}
-      <div class="ui_row">
-        <label class="dialog_row">${text}</label>
-      </div>
-      <div class="ui_row">
-        <div></div>
-        <div class="ui_btn_row">
-          <button id="dia_yes" class="ui_btn ui_btn_mini">${lang.pop_yes}</button>
-          <button id="dia_no" class="ui_btn ui_btn_mini">${lang.pop_no}</button>
-        </div>
-      </div>
-    </div>`;
-
-    EL('dia_yes').onclick = () => {
-      document.body.removeChild(box);
-      resolve(true);
-    };
-    EL('dia_no').onclick = () => {
-      document.body.removeChild(box);
-      resolve(false);
-    };
+    let box = makeDialog(title, text, [
+      {
+        text: lang.pop_yes,
+        click:() => {
+          document.body.removeChild(box);
+          resolve(true);
+        }
+      },
+      {
+        text: lang.pop_no,
+        click:() => {
+          document.body.removeChild(box);
+          resolve(false);
+        }
+      }
+    ]);
   });
 }
 
 function asyncPrompt(text, placeh = '', title = null) {
   return new Promise(resolve => {
-    let box = makeDialog();
-    let tit = title ? `<div class="ui_row ui_head">${title}</div>` : '';
-    box.innerHTML = `
-    <div class="ui_col ui_dialog">
-      ${tit}
-      <div class="ui_row">
-        <label class="dialog_row">${text}</label>
-      </div>
-      <div class="ui_row">
-        <input id="dia_input" class="ui_inp" type="text" value="${placeh}">
-      </div>
-      <div class="ui_row">
-        <div></div>
-        <div class="ui_btn_row">
-          <button id="dia_ok" class="ui_btn ui_btn_mini">OK</button>
-          <button id="dia_cancel" class="ui_btn ui_btn_mini">${lang.cancel}</button>
-        </div>
-      </div>
-    </div>`;
-
-    EL('dia_ok').onclick = () => {
-      let res = EL('dia_input').value;
-      document.body.removeChild(box);
-      resolve(res);
-    };
-    EL('dia_cancel').onclick = () => {
-      document.body.removeChild(box);
-      resolve(null);
-    };
+    let box = makeDialog(title, text, [
+      {
+        text: 'OK',
+        click:() => {
+          let res = EL('dia_input').value;
+          document.body.removeChild(box);
+          resolve(res);
+        }
+      },
+      {
+        text: lang.cancel,
+        click:() => {
+          document.body.removeChild(box);
+          resolve(null);
+        }
+      }
+    ], `<input id="dia_input" class="ui_inp" type="text" value="${placeh}">`);
   });
 }
